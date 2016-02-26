17a114190de6ed6fc0b238e8672d96269a1cd8c5
Tom <redsoxfantom@gmail.com>
2016-02-25 20:56:28 -0500

    Work on reading an obj file

--- RendererTest/Models/Loader/ObjModelFileReader.cs
+++ RendererTest/Models/Loader/ObjModelFileReader.cs
@@ -1,20 +1,55 @@
 ﻿using System.IO;
 using RendererTest.Elements.Models;
 using log4net;
+using System;
 
 namespace RendererTest.Models.Loader
 {
     public class ObjModelFileReader : IModelFileReader
     {
-        private ILog logger = LogManager.GetLogger("ObjModelFileReader");
+        Model loadedModel;
 
         public Model LoadFromFile(StreamReader file)
         {
-            Model loadedModel = new Model();
+            loadedModel = new Model();
 
-            file.
+            while(!file.EndOfStream)
+            {
+                string line = file.ReadLine();
+                if(line.StartsWith("v"))
+                {
+                    handleAddVertex(line);
+                }
+                if(line.StartsWith("f"))
+                {
+                    handleAddFace(line);
+                }
+            }
 
             return loadedModel;
+        }
+
+        private void handleAddFace(string line)
+        {
+            string[] splitLine = line.Split(' ');
+        }
+
+        private void handleAddVertex(string line)
+        {
+            // Vertices are defined as follows:
+            // 'v' x y z [w]
+            string[] splitLine = line.Split(' ');
+
+            double x = double.Parse(splitLine[1]);
+            double y = double.Parse(splitLine[2]);
+            double z = double.Parse(splitLine[3]);
+            double w = 1.0;
+            if(splitLine.Length > 4)
+            {
+                w = double.Parse(splitLine[4]);
+            }
+
+            loadedModel.AddVertex(x, y, z, w);
         }
     }
 }
