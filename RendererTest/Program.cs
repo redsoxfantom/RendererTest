e27bfd70dc6326e4d11ba1249485f7e6761689ad
Tom <redsoxfantom@gmail.com>
2016-02-27 14:27:37 -0500

    Added f 18 model

--- RendererTest/Program.cs
+++ RendererTest/Program.cs
@@ -26,7 +26,7 @@ namespace RendererTest
                     game.VSync = VSyncMode.On;
 
                     GL.EnableClientState(ArrayCap.VertexArray);
-                    model = loader.LoadModel("cube.obj");
+                    model = loader.LoadModel("planet.obj");
                 };
 
                 game.Resize += (sender, e) =>
