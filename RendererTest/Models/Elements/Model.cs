16d76998574406a72e5cc8aa797b655b9c37cf64
Tom <redsoxfantom@gmail.com>
2016-03-01 22:24:29 -0500

    Work on adding normals

--- RendererTest/Models/Elements/Model.cs
+++ RendererTest/Models/Elements/Model.cs
@@ -32,14 +32,14 @@ namespace RendererTest.Elements.Models
             vertices.Add(v3);
             numVerts += 3;
 
-            //vertices.Add(n1);
-            //vertices.Add(n2);
-            //vertices.Add(n3);
+            vertices.Add(n1);
+            vertices.Add(n2);
+            vertices.Add(n3);
         }
 
         public void Commit()
         {
-            logger.InfoFormat("Creating model with {0} vertices", vertices.Count);
+            logger.InfoFormat("Creating model with {0} vertices", numVerts);
 
             vbo = GL.GenBuffer();
 
@@ -65,9 +65,9 @@ namespace RendererTest.Elements.Models
             program.SetVariable("MVP", MVPmatrix); // Send the current Model-View-Projection matrix
             GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
             GL.EnableVertexAttribArray(0);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, (Vector3.SizeInBytes*6), 0);
             GL.EnableVertexAttribArray(1);
-            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, (Vector3.SizeInBytes * 6), Vector3.SizeInBytes*3);
 
             GL.DrawArrays(PrimitiveType.Triangles, 0, numVerts); // render the model
 
